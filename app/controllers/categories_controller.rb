f033a23c97670475da117ada25c5c4769b4bde9c
Sandy Cook <sandy.cook@rea-group.com>
2014-12-02 14:02:47 +1100

    Fix up the categories not being inactivated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -44,7 +44,7 @@ class CategoriesController < ApplicationController
     category.active = false
     category.save!
 
-    redirect_to category_path
+    redirect_to categories_path
   end
 
   private
