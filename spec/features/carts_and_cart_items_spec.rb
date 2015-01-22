285bdd7096441675aa50fa9144d5262e1eeef629
Sandy Cook <sandy.cook@rea-group.com>
2015-01-22 17:22:34 +1100

    feature spec for mini-cart

--- spec/features/carts_and_cart_items_spec.rb
+++ spec/features/carts_and_cart_items_spec.rb
@@ -3,8 +3,17 @@ require 'helpers/session_helper_spec'
 
 describe 'FEATURE cart and cart line items', js: true do
 
+  before(:each) do
+    @product_created = FactoryGirl.create(:category_with_products, :category_name => "Bob")
+  end
 
   it 'adds a product to the cart' do
+    visit(store_path)
+
+    first(:link, "Bob").click
+    first(:link, "Buy Now").click
+    expect(page).to have_css('#mini-cart', :text => 'You have 1 item(s) in your cart')
+    expect(page).to have_css('#mini-cart', :text => "19.99")
 
   end
 
